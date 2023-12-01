63b44cc5b91d0ed0daf259a932f8fa47680cb8f7
Rustem Salimov <rustem.salimov@kzn.akvelon.com>
2023-12-01 09:23:16 +0300

    [add] project

---
+++ Summarize/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Summarize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add("1,2,3,4"));
+        }
+
+        public static int Add(string numbers)
+        {
+            var parts = numbers.Split(',');
+            var result = 0;
+
+            foreach (var part in parts)
+            {
+                int.TryParse(part, out int number);
+
+                if (number <= 1000)
+                    result += number;
+            }
+
+            return result;
+        }
+    }
+}
